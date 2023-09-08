b34a058e61a3a1cb10a3f64b8d4b559e0818ac6d
Alexander Zinovev <a.zinovev@hotmail.com>
2023-09-08 16:28:19 +0200

    Scraping the billboard hot 100 (cleaned)

--- main.py
+++ main.py
@@ -2,47 +2,49 @@ from bs4 import BeautifulSoup
 import datetime as dt
 import requests
 
+# Constants
+BASE_URL = 'https://www.billboard.com/charts/hot-100/'
 
 today = dt.date.today()
-date_today = today.strftime("%Y-%m-%d")
 
-
+# Function to validate date formate
 def is_date_format_valid(date_input):
-    if len(date_input) != 10 or \
-        date_input[4] and date_input [7] != "-" or \
-        date_input.replace("-", "").isdigit() == False:
-            print("\n❌ The input is invalid. Use the required date format! \n")
-            return False
-
-    input_year = int(date_input[:4])
-    input_month = int(date_input[5:7])
-    input_day = int(date_input[8:])
-    if dt.date(input_year, input_month, input_day) > today:
-        print("\n❌ The picked date is a day in the future. Try again! \n")
+    try:
+        dt_input_obj = dt.datetime.strptime(date_input, "%Y-%m-%d")
+        return dt_input_obj.date() <= dt.date.today()
+    except ValueError:
         return False
-    else:
-        return True
 
 
-date_valid = False
+def get_valid_date():
+    while True:
+        picked_date = input("📅 Pick a date in the past for time traveling! \
+            \nHint: Use the following format (YYYY-MM-DD) ➡️ ")
+        if is_date_format_valid(picked_date):
+            return picked_date
+        else:
+            print("\n❌ Invalid date format or future date. Try again! \n")
 
-while not date_valid:
-    picked_date = input("📅 Pick a date in the past for time traveling! \
-        \nHint: Use the following format (YYYY-MM-DD) ➡️ ")
-    date_valid = is_date_format_valid(picked_date)
 
-print(f"\n✅ Picked date: {picked_date}\n")
+def main():
+    picked_date = get_valid_date()
+    print(f"\n✅ Picked date: {picked_date}\n")
 
-base_url = 'https://www.billboard.com/charts/hot-100/'
-response = requests.get(base_url + picked_date)
-chart_page = response.text
+    response = requests.get(BASE_URL + picked_date)
+    if response.status_code != 200:
+        print(f"❌ Error: {response.status_code}")
+        return
 
-soup = BeautifulSoup(chart_page, 'html.parser')
+    chart_page = response.text
+    soup = BeautifulSoup(chart_page, 'html.parser')
 
-print(soup.title)
+    # song_titles = soup.select(selector='div.o-chart-results-list-row-container ul li:nth-of-type(4) ul li:first-of-type span')
+    song_titles = soup.select(selector='li ul li h3')
 
-song_titles = soup.select(selector='div.o-chart-results-list-row-container ul li:nth-of-type(4) ul li:first-of-type span')
+    for song in song_titles:
+        for string in song.stripped_strings:
+            print(string)
 
-for song in song_titles:
-    for string in song.stripped_strings:
-        print(string)
+
+if __name__ == '__main__':
+    main()
